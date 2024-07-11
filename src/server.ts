fd59964ab3cb29725fcd5a62c8a558c242565420
emerson <emerson8096@gmail.com>
2024-07-11 10:12:41 -0300

    update server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
 import { createTrip } from "./routes/create-trip";
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
 
 const app = fastify();
+
+app.register(cors, {
+  origin: "http://localhost:3333",
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.register(createTrip)
+app.register(createTrip);
+app.register(confirmTrip);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running");
