fb70aa75c8a732824e1c7ea14b7afe865956e9d5
emerson <emerson8096@gmail.com>
2024-07-16 15:37:36 -0300

    update confirm-participante

--- src/routes/confirm-participant.ts
+++ src/routes/confirm-participant.ts
@@ -2,6 +2,8 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { ClientError } from "../errors/client-error";
+import { env } from "../env";
 
 export async function confirmParticipant(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
@@ -23,11 +25,11 @@ export async function confirmParticipant(app: FastifyInstance) {
       });
 
       if (!participant) {
-        throw new Error("Participant not found.");
+        throw new ClientError("Participant not found.");
       }
 
       if (participant.is_confirmed) {
-        reply.redirect(`http://localhost:3000/trips/${participant.trip_id}`);
+        reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_id}`);
       }
 
       await prisma.participant.update({
@@ -36,7 +38,7 @@ export async function confirmParticipant(app: FastifyInstance) {
       });
 
       return reply.redirect(
-        `http://localhost:3000/trips/${participant.trip_id}`
+        `${env.WEB_BASE_URL}/trips/${participant.trip_id}`
       );
     }
   );
